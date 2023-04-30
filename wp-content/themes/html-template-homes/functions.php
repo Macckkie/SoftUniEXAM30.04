2546fc9f68b9e198747d4b8a4b84b7b3af5b377a
Mihail Peev <mihail.peev.mp@gmail.com>
2023-04-30 12:09:25 +0300

    Theme add update

--- wp-content/themes/html-template-homes/functions.php
+++ wp-content/themes/html-template-homes/functions.php
@@ -1,6 +1,6 @@
 <?php
 //this function here takes care of handling the assets with enqueue
 function softuni_assets(){
-wp_enqueue_style('html-template-homes',get_stylesheet_directory_uri() . '/assets/css/master.css', array(), '0.1.0');
+wp_enqueue_style('html-template-homes',get_stylesheet_directory_uri() . '/assets/css/master.css', array(), '0.1.2');
 }
 add_action('wp_enqueue_scripts', 'softuni_assets'); 
